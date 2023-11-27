d1a5cbd9cb87a34676dfb279d2edb179077f941b
Håvard Fjær <github@fjaer.com>
2023-11-27 18:11:47 +0100

    ggrep and kill logic

--- midi-startup.sh
+++ midi-startup.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+# stop script
+if [ "$1" = "stop" ]
+then
+    echo "Stopping script..."
+    kill $(pgrep -f mix_es9_from_korg.py)
+    exit 0
+fi
+
 if pgrep -f mix_es9_from_korg.py > /dev/null
 then
-    echo "Script already running."
+    echo "Script already running with PID $(pgrep -f mix_es9_from_korg.py)"
 else
     echo "Starting script..."
-    python3 mix_es9_from_korg.py
+    python3 mix_es9_from_korg.py &
 fi
